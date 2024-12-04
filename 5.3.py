63e1fd21e7e095b7fb9af3ab5b80f458692660d0
hoangvanluc3305 <hluc67812@gmail.com>
2024-12-04 09:14:14 +0700

    Add files via upload

---
+++ 5.3.py
@@ -0,0 +1,6 @@
+print("HOÀNG VĂN LỰC")
+print("MSSV:235752021610065")
+
+import numpy as np 
+x = np.arange(12, 38) 
+print(x) 
---
+++ 5.4.py
@@ -0,0 +1,9 @@
+print("HOÀNG VĂN LỰC")
+print("MSSV:235752021610065")
+
+import numpy as np
+mang = np.arange(12, 39)
+print("Mảng ban đầu:",mang)
+
+mang_dao_nguoc = np.flip(mang)
+print("Mảng đảo ngược:", mang_dao_nguoc)
